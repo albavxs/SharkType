0ca9a5adfb591f45c34d04497bdada4bf4358662
Paulo Guilherme Santos Alves <126200347+albavxs@users.noreply.github.com>
2026-09-12 22:29:46 -0300

    fix(landing): update community destinations

--- lib/community.ts
+++ lib/community.ts
@@ -1,5 +1,5 @@
 export const COMMUNITY_LINKS = {
-  discord: 'https://discord.gg/mStvXxbmG',
-  github: 'https://github.com/Few-Soldiers',
+  discord: 'https://discord.gg/M8sFAGZhd',
+  github: 'https://github.com/albavxs',
   website: 'https://www.fewcompany.com/pt',
 } as const
